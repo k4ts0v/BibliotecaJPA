425aa18aa4e3c2c680e4d34b49cc12826727c777
Lucas V. <77939384+k4ts0v@users.noreply.github.com>
2024-12-11 12:46:55 +0100

    refactor: borrado de imports y código comentado.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,13 @@
 
 
-import modelo.dao.DAOPrestamo;
-import modelo.dto.Ejemplar;
-import modelo.dto.Libro;
-import modelo.dto.Prestamo;
-import modelo.dto.Usuario;
-import modelo.gestion.GestionEjemplar;
-import modelo.gestion.GestionLibro;
-import modelo.gestion.GestionPrestamo;
-import modelo.gestion.GestionUsuario;
 import modelo.menus.MenuPrincipal;
 
 import java.sql.SQLException;
-import java.time.LocalDate;
 
 public class Main {
     static MenuPrincipal menu = new MenuPrincipal();
 
     public static void main(String[] args) throws SQLException {
-//        Usuario usuario = new Usuario("23342184A", "Foo", "foobar@mail.co", "P455w0rd!", "normal", null);
-//        gestionUsuario.createUsuario(usuario);
-//        gestionUsuario.createUsuario(new Usuario("23342184A", "Foo", "foobar@mail.co", "P455w0rd!", "Administrador", null));
-//        gestionLibro.createLibro(new Libro("9781486010974", "Título", "Autor"));
-//        gestionLibro.createLibro(new Libro("978-0-522-86467-0", "Otro título", "Otro autor"));
-//        Ejemplar ejemplar = new Ejemplar(new Libro("978-1-4860-1097-4", "Título", "Autor"), "Disponible");
-//        gestionEjemplar.createEjemplar(ejemplar);
-//        gestionEjemplar.createEjemplar(new Ejemplar(new Libro("978-1-4860-1097-4", "Título", "Autor"), "Prestado"));
-//        Prestamo prestamo = new Prestamo(usuario, ejemplar, LocalDate.now());
-//        System.out.println(gestionPrestamo.createPrestamo(prestamo));
-//        gestionPrestamo.readAllPrestamos();
         menu.run();
     }
 
